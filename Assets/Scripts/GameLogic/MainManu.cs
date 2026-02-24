721e341c18d6099e75bc63b5d31b1c988acf05e2
Alexia Martínez <amartinezla@edu.tecnocampus.cat>
2026-02-24 18:32:52 +0100

    Fixes

--- Assets/Scripts/GameLogic/MainManu.cs
+++ Assets/Scripts/GameLogic/MainManu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
@@ -6,5 +7,12 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("Gameplay");
+    }
+    public void Update()
+    {
+        if ((Keyboard.current != null) && Keyboard.current.enterKey.wasPressedThisFrame)
+        {
+            StartGame();
+        }
     }
 }
