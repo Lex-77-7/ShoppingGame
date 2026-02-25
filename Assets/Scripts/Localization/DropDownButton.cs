3b762f146c5879d1374f36aa038ecfc677e6338b
Alexia Martínez <amartinezla@edu.tecnocampus.cat>
2026-02-25 17:50:05 +0100

    drop down button

---
+++ Assets/Scripts/Localization/DropDownButton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DropDownButton : MonoBehaviour
+{
+    public Language Language;
+    public void DownButton(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                Language = Language.English;
+                break;
+            case 1:
+                Language = Language.Català;
+                break;
+            case 2:
+                Language = Language.Español;
+                break;     
+        }
+
+        Localizer.SetLanguage(Language);
+    }
+}
